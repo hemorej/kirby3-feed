eb1554427c3745d6d022defd1c67807f4048c625
jharfouche <jharfouche@toonboom.com>
2019-12-04 15:53:20 -0500

    fix closing img tag

--- snippets/feed/rss.php
+++ snippets/feed/rss.php
@@ -13,7 +13,7 @@
         <link><?= \Kirby\Toolkit\Xml::encode($item->{$urlfield}()) ?></link>
         <guid><?= \Kirby\Toolkit\Xml::encode($item->url()) ?></guid>
         <pubDate><?= $datefield === 'modified' ? $item->modified('r', 'date') : date('r', $item->{$datefield}()->toTimestamp()) ?></pubDate>
-        <?php $description = "<img alt='journal for " . $item->{$titlefield}() . "' src='" . $item->images()->first()->url() . "' />"; ?>
+        <?php $description = "<img alt='journal for " . $item->{$titlefield}() . "' src='" . $item->images()->first()->url() . "'>"; ?>
         <description><?= \Kirby\Toolkit\Xml::encode($description) ?></description>
       </item>
   <?php endforeach; ?>
